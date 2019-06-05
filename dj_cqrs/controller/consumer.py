f6d44af2b83a2be7cc492ac91c0575438f2144fd
Max Kolyubyakin <Maxim.Kolyubyakin@ingrammicro.com>
2019-06-05 12:28:33 +0300

    Better consumer annotation.

--- dj_cqrs/controller/consumer.py
+++ dj_cqrs/controller/consumer.py
@@ -6,6 +6,6 @@ from dj_cqrs.factories import ReplicaFactory
 def consume(payload):
     """ Consumer controller.
 
-    :param TransportPayload payload: Consumed payload from master service.
+    :param dj_cqrs.dataclasses.TransportPayload payload: Consumed payload from master service.
     """
     ReplicaFactory.factory(payload.signal_type, payload.cqrs_id, payload.instance_data)
