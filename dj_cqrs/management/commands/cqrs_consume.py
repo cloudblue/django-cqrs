602a07308d106aadb9557c33b0d14897bdaf85af
Aleksandra Ovchinnikova <nsanjka@gmail.com>
2023-01-24 20:28:25 +0100

    Improve auto reload for cqrs_consume

--- dj_cqrs/management/commands/cqrs_consume.py
+++ dj_cqrs/management/commands/cqrs_consume.py
@@ -1,50 +1,116 @@
 #  Copyright © 2022 Ingram Micro Inc. All rights reserved.
-import multiprocessing
+import logging
 import signal
+import threading
+from pathlib import Path
 
 from django.core.management.base import BaseCommand, CommandError
+from watchfiles import watch
+from watchfiles.filters import PythonFilter
+from watchfiles.run import start_process
 
 from dj_cqrs.registries import ReplicaRegistry
-from dj_cqrs.transport import current_transport
+
+
+logger = logging.getLogger('django_cqrs.cqrs_consume')
+
+
+def consume(**kwargs):
+    import django
+    django.setup()
+
+    from dj_cqrs.transport import current_transport
+    current_transport.consume(**kwargs)
 
 
 class WorkersManager:
 
-    def __init__(self, options, transport, consume_kwargs):
+    def __init__(
+            self,
+            consume_kwargs,
+            workers=1,
+            reload=False,
+            ignore_paths=None,
+            sigint_timeout=5,
+            sigkill_timeout=1,
+    ):
         self.pool = []
-        self.options = options
-        self.transport = transport
+        self.workers = workers
+        self.reload = reload
         self.consume_kwargs = consume_kwargs
+        self.stop_event = threading.Event()
+        self.sigint_timeout = sigint_timeout
+        self.sigkill_timeout = sigkill_timeout
+
+        if self.reload:
+            self.watch_filter = PythonFilter(ignore_paths=ignore_paths)
+            self.watcher = watch(
+                Path.cwd(),
+                watch_filter=self.watch_filter,
+                stop_event=self.stop_event,
+                yield_on_timeout=True,
+            )
+
+    def handle_signal(self, *args, **kwargs):
+        self.stop_event.set()
+
+    def run(self):
+        for sig in [signal.SIGINT, signal.SIGTERM]:
+            signal.signal(sig, self.handle_signal)
+        if self.reload:
+            signal.signal(signal.SIGHUP, self.restart)
+
+        self.start()
+
+        if self.reload:
+            for files_changed in self:
+                if files_changed:
+                    self.restart()
+        else:
+            self.stop_event.wait()
+
+        self.terminate()
 
     def start(self):
-        for i in range(self.options['workers'] or 1):
-            process = multiprocessing.Process(
-                name=f'cqrs-consumer-{i}',
-                target=self.transport.consume,
-                kwargs=self.consume_kwargs,
+        for _ in range(self.workers):
+            process = start_process(
+                consume,
+                'function',
+                (),
+                self.consume_kwargs,
             )
             self.pool.append(process)
-            process.start()
-
-        for process in self.pool:
-            process.join()
 
     def terminate(self, *args, **kwargs):
         while self.pool:
-            p = self.pool.pop()
-            p.terminate()
-            p.join()
+            process = self.pool.pop()
+            process.stop(sigint_timeout=self.sigint_timeout, sigkill_timeout=self.sigkill_timeout)
 
-    def reload(self, *args, **kwargs):
+    def restart(self, *args, **kwargs):
         self.terminate()
         self.start()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        changes = next(self.watcher)
+        if changes:
+            return list({Path(c[1]) for c in changes})
+        return None
 
 
 class Command(BaseCommand):
     help = 'Starts CQRS worker, which consumes messages from message queue.'
 
     def add_arguments(self, parser):
-        parser.add_argument('--workers', '-w', help='Number of workers', type=int, default=0)
+        parser.add_argument(
+            '--workers',
+            '-w',
+            help='Number of workers',
+            type=int,
+            default=1,
+        )
         parser.add_argument(
             '--cqrs-id',
             '-cid',
@@ -53,35 +119,72 @@ class Command(BaseCommand):
             help='Choose model(s) by CQRS_ID for consuming',
         )
         parser.add_argument(
-            '--reload', '-r', help='Enable reload signal SIGHUP', action='store_true',
+            '--reload',
+            '-r',
+            help=(
+                'Enable reload signal SIGHUP and autoreload '
+                'on file changes'
+            ),
+            action='store_true',
+            default=False,
+        )
+        parser.add_argument(
+            '--ignore-paths',
+            nargs='?',
+            type=str,
+            help=(
+                'Specify directories to ignore, '
+                'to ignore multiple paths use a comma as separator, '
+                'e.g. "env" or "env,node_modules"'
+            ),
+        )
+        parser.add_argument(
+            '--sigint-timeout',
+            nargs='?',
+            type=int,
+            default=5,
+            help='How long to wait for the sigint timeout before sending sigkill.',
+        )
+        parser.add_argument(
+            '--sigkill-timeout',
+            nargs='?',
+            type=int,
+            default=1,
+            help='How long to wait for the sigkill timeout before issuing a timeout exception.',
         )
 
-    def handle(self, *args, **options):
-        if not options['workers'] and not options['reload']:
-            current_transport.consume(**self.get_consume_kwargs(options))
-            return
+    def handle(
+            self,
+            *args,
+            workers=1,
+            cqrs_id=None,
+            reload=False,
+            ignore_paths=None,
+            sigint_timeout=5,
+            sigkill_timeout=1,
+            **options,
+    ):
 
-        self.start_workers_pool(options)
+        paths_to_ignore = None
+        if ignore_paths:
+            paths_to_ignore = [Path(p).resolve() for p in ignore_paths.split(',')]
 
-    def start_workers_pool(self, options):
         workers_manager = WorkersManager(
-            options, current_transport, self.get_consume_kwargs(options),
+            workers=workers,
+            consume_kwargs=self.get_consume_kwargs(cqrs_id),
+            reload=reload,
+            ignore_paths=paths_to_ignore,
+            sigint_timeout=sigint_timeout,
+            sigkill_timeout=sigkill_timeout,
         )
-        if options['reload']:
-            try:
-                multiprocessing.set_start_method('spawn')
-            except RuntimeError:
-                pass
 
-            signal.signal(signal.SIGHUP, workers_manager.reload)
+        workers_manager.run()
 
-        workers_manager.start()
-
-    def get_consume_kwargs(self, options):
+    def get_consume_kwargs(self, ids_list):
         consume_kwargs = {}
-        if options.get('cqrs_id'):
+        if ids_list:
             cqrs_ids = set()
-            for cqrs_id in options['cqrs_id']:
+            for cqrs_id in ids_list:
                 model = ReplicaRegistry.get_model_by_cqrs_id(cqrs_id)
                 if not model:
                     raise CommandError('Wrong CQRS ID: {0}!'.format(cqrs_id))
--- tests/test_commands/test_consume.py
+++ tests/test_commands/test_consume.py
@@ -1,12 +1,13 @@
 #  Copyright © 2021 Ingram Micro Inc. All rights reserved.
 
+import threading
 from importlib import import_module, reload
+from pathlib import Path
 
 import pytest
 from django.core.management import CommandError, call_command
 
-from dj_cqrs.management.commands.cqrs_consume import WorkersManager
-from dj_cqrs.transport import current_transport
+from dj_cqrs.management.commands.cqrs_consume import WorkersManager, consume
 
 
 COMMAND_NAME = 'cqrs_consume'
@@ -18,41 +19,54 @@ def reload_transport():
 
 
 def test_no_arguments(mocker, reload_transport):
-    consume_mock = mocker.patch('tests.dj.transport.TransportStub.consume')
+    mocked_worker = mocker.patch('dj_cqrs.management.commands.cqrs_consume.WorkersManager')
 
     call_command(COMMAND_NAME)
 
-    consume_mock.assert_called_once_with()
+    mocked_worker.assert_called_once_with(
+        consume_kwargs={},
+        workers=1,
+        reload=False,
+        ignore_paths=None,
+        sigint_timeout=5,
+        sigkill_timeout=1,
+    )
 
 
-def test_several_workers(reload_transport):
-    call_command(COMMAND_NAME, '--workers=2')
+def test_with_arguments(mocker, reload_transport):
+    mocked_worker = mocker.patch('dj_cqrs.management.commands.cqrs_consume.WorkersManager')
+    mocker.patch(
+        'dj_cqrs.management.commands.cqrs_consume.Path',
+        side_effect=[
+            mocker.MagicMock(resolve=mocker.MagicMock(return_value='/path1')),
+            mocker.MagicMock(resolve=mocker.MagicMock(return_value='/path2')),
+        ],
+    )
 
-
-def test_reload_flag(reload_transport):
-    call_command(COMMAND_NAME, '--reload')
-
-
-def test_workers_manager(reload_transport):
-    manager = WorkersManager({'workers': 1}, current_transport, {})
-    manager.start()
-    manager.reload()
-
-
-def test_one_worker_one_cqrs_id(mocker, reload_transport):
-    consume_mock = mocker.patch('dj_cqrs.management.commands.cqrs_consume.WorkersManager.start')
-
-    call_command(COMMAND_NAME, '--workers=1', '-cid=author')
-
-    consume_mock.assert_called_once_with()
+    call_command(COMMAND_NAME, '--workers=2', '-r', '-cid=author', '--ignore-paths=path1,path2')
+    mocked_worker.assert_called_once_with(
+        consume_kwargs={'cqrs_ids': {'author'}},
+        workers=2,
+        reload=True,
+        ignore_paths=['/path1', '/path2'],
+        sigint_timeout=5,
+        sigkill_timeout=1,
+    )
 
 
 def test_several_cqrs_id(mocker, reload_transport):
-    consume_mock = mocker.patch('tests.dj.transport.TransportStub.consume')
+    mocked_worker = mocker.patch('dj_cqrs.management.commands.cqrs_consume.WorkersManager')
 
     call_command(COMMAND_NAME, cqrs_id=['author', 'basic', 'author', 'no_db'])
 
-    consume_mock.assert_called_once_with(cqrs_ids={'author', 'basic', 'no_db'})
+    mocked_worker.assert_called_once_with(
+        consume_kwargs={'cqrs_ids': {'author', 'basic', 'no_db'}},
+        workers=1,
+        reload=False,
+        ignore_paths=None,
+        sigint_timeout=5,
+        sigkill_timeout=1,
+    )
 
 
 def test_wrong_cqrs_id(reload_transport):
@@ -60,3 +74,100 @@ def test_wrong_cqrs_id(reload_transport):
         call_command(COMMAND_NAME, cqrs_id=['author', 'random', 'no_db'])
 
     assert "Wrong CQRS ID: random!" in str(e)
+
+
+def test_worker_manager_constructor_with_reload(mocker):
+    mocked_flt_instance = mocker.MagicMock()
+    mocked_pyfilter = mocker.patch(
+        'dj_cqrs.management.commands.cqrs_consume.PythonFilter',
+        return_value=mocked_flt_instance,
+    )
+    mocked_watch = mocker.patch('dj_cqrs.management.commands.cqrs_consume.watch')
+
+    worker = WorkersManager(
+        {},
+        reload=True,
+        sigint_timeout=10,
+    )
+
+    assert worker.workers == 1
+    assert worker.reload is True
+    assert worker.sigint_timeout == 10
+    assert worker.sigkill_timeout == 1
+    assert isinstance(worker.stop_event, threading.Event)
+
+    mocked_pyfilter.assert_called_once_with(ignore_paths=None)
+    assert worker.watch_filter == mocked_flt_instance
+    mocked_watch.assert_called_once_with(
+        Path.cwd(),
+        watch_filter=mocked_flt_instance,
+        stop_event=worker.stop_event,
+        yield_on_timeout=True,
+    )
+
+
+def test_worker_manager_run_no_reload(mocker):
+    mocked_start_process = mocker.patch('dj_cqrs.management.commands.cqrs_consume.start_process')
+
+    worker = WorkersManager(
+        {'cqrs_ids': {'author', 'basic', 'no_db'}},
+        workers=2,
+    )
+    worker.stop_event.wait = mocker.MagicMock()
+
+    worker.run()
+
+    mocked_start_process.assert_called()
+
+
+def test_worker_manager_run_with_reload(mocker):
+    mocker.patch.object(
+        WorkersManager,
+        '__next__',
+        side_effect=[[Path.cwd() / Path('file1.py')], None],
+    )
+    mocked_start_process = mocker.patch('dj_cqrs.management.commands.cqrs_consume.start_process')
+
+    worker = WorkersManager(
+        {'cqrs_ids': {'author', 'basic', 'no_db'}},
+        reload=True,
+    )
+    worker.stop_event.wait = mocker.MagicMock()
+
+    worker.run()
+
+    mocked_start_process.assert_called()
+
+
+def test_worker_manager_handle_signal():
+    worker = WorkersManager({})
+    worker.handle_signal()
+
+    assert worker.stop_event.is_set()
+
+
+def test_worker_manager_iterator():
+    worker = WorkersManager({})
+
+    worker.watcher = iter([[(None, '/file1.py')], None])
+
+    expected_result = [[Path('/file1.py')], None]
+    result = []
+
+    for file in worker:
+        result.append(file)
+
+    assert result == expected_result
+
+
+def test_consume(mocker):
+    mocked_setup = mocker.patch('django.setup')
+    mocked_consume = mocker.patch(
+        'dj_cqrs.transport.current_transport.consume',
+    )
+
+    consume_kwargs = {'cqrs_ids': {'author', 'basic', 'no_db'}}
+    consume(**consume_kwargs)
+
+    mocked_setup.assert_called_once()
+    mocked_consume.assert_called_once_with(**consume_kwargs)
